6328234959635c4b26a9032f17f1836fd5bd3323
lehuuhieu-0805 <01643815296h@gmail.com>
2023-02-16 13:10:37 +0700

    add phone entity

---
+++ src/components/phone/phone.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('phones')
+export class Phone {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  quantity: number;
+
+  @Column()
+  image: string;
+}
