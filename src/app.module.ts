871fbf336d1af652f5a22759a7ac2bb3db79ea55
lehuuhieu-0805 <01643815296h@gmail.com>
2023-02-22 09:48:26 +0700

    feat: add config redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,42 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+/* eslint-disable prettier/prettier */
+import { CacheModule, CacheStore, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './components/user/user.module';
-import { dataSourceOptions } from './config/data-source.config';
+import { redisStore } from 'cache-manager-redis-store';
 import { AuthModule } from './components/auth/auth.module';
 import { PhoneModule } from './components/phone/phone.module';
+import { UserModule } from './components/user/user.module';
+import { dataSourceOptions } from './config/data-source.config';
+import { CartModule } from './components/cart/cart.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const store = await redisStore({
+          socket: {
+            host: configService.get('REDIS_HOST'),
+            port: configService.get('REDIS_PORT'),
+          },
+        });
+
+        return {
+          store: store as unknown as CacheStore,
+          ttl: 0,
+        };
+      },
+    }),
     TypeOrmModule.forRoot(dataSourceOptions),
     UserModule,
     AuthModule,
     PhoneModule,
+    CartModule,
   ],
 })
 export class AppModule {}
