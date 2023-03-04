3657447710091b66f4fe8f0d1f1208152a9038c0
lehuuhieu-0805 <01643815296h@gmail.com>
2023-03-04 08:51:41 +0700

    refactor: change filename extenstion

--- src/components/order-item/order-item.module.ts
+++ src/components/order-item/order-item.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderItemRepository } from './../../repositories/order-item.repository';
 import { OrderModule } from './../order/order.module';
 import { PhoneModule } from './../phone/phone.module';
-import { ORDER_ITEM_REPOSITORY } from './interfaces/order-item.repository';
+import { ORDER_ITEM_REPOSITORY } from './interfaces/order-item.repository.interface';
 import { ORDER_ITEM_SERVICE } from './interfaces/order-item.service.interface';
 import { OrderItemController } from './order-item.controller';
 import { OrderItem } from './order-item.entity';
