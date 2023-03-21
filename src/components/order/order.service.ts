b7ee8b088ff3409b5f4bc410abdda7bc6658ace6
lehuuhieu-0805 <01643815296h@gmail.com>
2023-03-21 15:47:25 +0700

    feat: use transaction in sql to create order

--- src/components/order/order.service.ts
+++ src/components/order/order.service.ts
@@ -1,14 +1,3 @@
-import { StatusEnum } from './../../constants/common';
-import { OrderItem } from './../order-item/order-item.entity';
-import { Phone } from './../phone/phone.entity';
-import {
-  PHONE_REPOSITORY,
-  IPhoneRepository,
-} from './../phone/interfaces/phone.repository.interface';
-import {
-  ORDER_ITEM_REPOSITORY,
-  IOrderItemRepository,
-} from './../order-item/interfaces/order-item.repository.interface';
 import {
   CACHE_MANAGER,
   HttpException,
@@ -16,6 +5,19 @@ import {
   Inject,
   Injectable,
 } from '@nestjs/common';
+import { Cache } from 'cache-manager';
+import { DataSource } from 'typeorm';
+import { StatusEnum } from './../../constants/common';
+import {
+  IOrderItemRepository,
+  ORDER_ITEM_REPOSITORY,
+} from './../order-item/interfaces/order-item.repository.interface';
+import { OrderItem } from './../order-item/order-item.entity';
+import {
+  IPhoneRepository,
+  PHONE_REPOSITORY,
+} from './../phone/interfaces/phone.repository.interface';
+import { Phone } from './../phone/phone.entity';
 import { User } from './../user/user.entity';
 import { CreateOrderDto } from './dto/create-order.dto';
 import {
@@ -24,7 +26,6 @@ import {
 } from './interfaces/order.repository.interface';
 import { IOrderService } from './interfaces/order.service.interface';
 import { Order } from './order.entity';
-import { Cache } from 'cache-manager';
 
 @Injectable()
 export class OrderService implements IOrderService {
@@ -40,6 +41,8 @@ export class OrderService implements IOrderService {
 
     @Inject(CACHE_MANAGER)
     private readonly cacheManager: Cache,
+
+    private dataSource: DataSource,
   ) {}
 
   async getById(id: string, user: User): Promise<Order> {
@@ -58,55 +61,81 @@ export class OrderService implements IOrderService {
       throw new HttpException('Cart is empty', HttpStatus.BAD_REQUEST);
     }
 
-    for (const cartItem of cart) {
-      const phone = await this.phoneRepository.findById(cartItem.id);
-      if (phone.quantity < cartItem.quantity) {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      const order = new Order();
+      order.totalPrice = createOrderDto.totalPrice;
+      order.totalQuantity = createOrderDto.totalQuantity;
+      order.dateCreated = new Date();
+      order.status = 'SUCCESS';
+      order.user = user;
+
+      // const orderCreated = await this.orderRepository.create(order);
+      const orderCreated = await queryRunner.manager.save(order);
+
+      for (const cartItem of cart) {
+        // const phone = await this.phoneRepository.findById(cartItem.id);
+        const phone = await queryRunner.manager.findOneBy(Phone, {
+          id: cartItem.id,
+        });
+        phone.quantity -= cartItem.quantity;
+        console.log(phone.quantity);
+        if (phone.quantity < 0) {
+          await queryRunner.rollbackTransaction();
+          throw new HttpException(
+            'Quantity of phone is not enough',
+            HttpStatus.BAD_REQUEST,
+          );
+        } else if (phone.quantity === 0) {
+          phone.status = StatusEnum.INACTIVE;
+        }
+        try {
+          // await this.phoneRepository.updatePhone(phone.id, phone);
+          await queryRunner.manager.update(Phone, phone.id, phone);
+        } catch (error) {
+          await queryRunner.rollbackTransaction();
+          throw new HttpException(
+            'Update phone failed',
+            HttpStatus.BAD_REQUEST,
+          );
+        }
+
+        const orderItem = new OrderItem();
+        orderItem.quantity = cartItem.quantity;
+        orderItem.price = cartItem.price;
+        orderItem.phone = phone;
+        orderItem.order = orderCreated;
+
+        try {
+          // await this.orderItemRepository.create(orderItem);
+          await queryRunner.manager.save(orderItem);
+
+          await this.cacheManager.del(`cart:${user.username}`);
+        } catch (error) {
+          await queryRunner.rollbackTransaction();
+          throw new HttpException(
+            'Create order item failed',
+            HttpStatus.BAD_REQUEST,
+          );
+        }
+        await queryRunner.commitTransaction();
+        return orderCreated;
+      }
+    } catch (error) {
+      if (error instanceof HttpException) {
+        throw new HttpException(error.message, error.getStatus());
+      } else {
+        await queryRunner.rollbackTransaction();
         throw new HttpException(
-          'Quantity of phone is not enough',
-          HttpStatus.BAD_REQUEST,
+          'Create order failed',
+          HttpStatus.INTERNAL_SERVER_ERROR,
         );
       }
+    } finally {
+      await queryRunner.release();
     }
-
-    const order = new Order();
-    order.totalPrice = createOrderDto.totalPrice;
-    order.totalQuantity = createOrderDto.totalQuantity;
-    order.dateCreated = new Date();
-    order.status = 'SUCCESS';
-    order.user = user;
-
-    const orderCreated = await this.orderRepository.create(order);
-
-    for (const cartItem of cart) {
-      const phone = await this.phoneRepository.findById(cartItem.id);
-      phone.quantity -= cartItem.quantity;
-      if (phone.quantity === 0) {
-        phone.status = StatusEnum.INACTIVE;
-      }
-      try {
-        await this.phoneRepository.updatePhone(phone.id, phone);
-      } catch (error) {
-        throw new HttpException('Update phone failed', HttpStatus.BAD_REQUEST);
-      }
-
-      const orderItem = new OrderItem();
-      orderItem.quantity = cartItem.quantity;
-      orderItem.price = cartItem.price;
-      orderItem.phone = phone;
-      orderItem.order = orderCreated;
-
-      try {
-        await this.orderItemRepository.create(orderItem);
-
-        await this.cacheManager.del(`cart:${user.username}`);
-      } catch (error) {
-        throw new HttpException(
-          'Create order item failed',
-          HttpStatus.BAD_REQUEST,
-        );
-      }
-    }
-
-    return orderCreated;
   }
 }
--- src/config/data-source.config.ts
+++ src/config/data-source.config.ts
@@ -25,4 +25,13 @@ export const dataSourceOptions: DataSourceOptions = {
 
 const dataSource = new DataSource(dataSourceOptions);
 
+dataSource
+  .initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization', err);
+  });
+
 export default dataSource;
